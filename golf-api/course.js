03ada69f5d30e3e3d6fdc84ed24fc5cafad35f78
Ry <ryan.hixon@gmail.com>
2026-01-21 21:38:51 -0500

    add post for new tee

--- golf-api/course.js
+++ golf-api/course.js
@@ -12,6 +12,36 @@ async function postCourse(name, city, stateid, zip, address, ncrdb){
     });
 }
 
+async function postTee(courseid, name, color, text, numberofholes, rating, slope, par, 
+            front9rating, front9slope, front9par, back9rating, back9slope, back9par){
+    return new Promise((resolve, reject) => {
+        connection.query('INSERT INTO tee (courseid, name, color, text, numberofholes, rating, slope, par, front9rating, front9slope, front9par, back9rating, back9slope, back9par) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', 
+            [courseid, name, color, text, numberofholes, rating, slope, par, front9rating, front9slope, front9par, back9rating, back9slope, back9par], (err) => { 
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve();
+                }
+            });
+    });
+
+}
+
+async function getCourseTees(courseid){
+    return new Promise((resolve, reject) => {
+        connection.query('SELECT from tee WHERE courseid = (?) and active = true', [courseid], (err, rows) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(rows);
+            }
+        });
+    });
+}
+
 export default {
-    postCourse
+    postCourse,
+    postTee,
+
+    getCourseTees
 }
--- golf-api/server.js
+++ golf-api/server.js
@@ -4,8 +4,6 @@ import cors from 'cors';
 import player from './player.js';
 import course from './course.js';
 
-//connection.connect();
-
 const app = express();
 app.use(cors({
     origin: process.env.UI_URL,
@@ -48,5 +46,32 @@ app.post(`${home}/course`, async function(req, res){
     res.status(201).json(req.body);
 });
 
-//connection.end();
+app.post(`${home}/tee/{:slug}`, async function(req, res){
+    console.log('POST new TEES');
+    const tee = req.body;
+    const courseid = req.params.slug;
+
+    console.log(tee);
+    console.log(courseid);
+    
+    const name = tee.name;
+    const color = tee.color;
+    const text = tee.text;
+    const numberofholes = tee.numberofholes;
+    const rating = tee.rating;
+    const slope = tee.slope;
+    const par = tee.par;
+    const front9rating = tee.front9rating;
+    const front9slope = tee.front9slope;
+    const front9par = tee.front9par;
+    const back9rating = tee.back9rating;
+    const back9slope = tee.back9slope;
+    const back9par = tee.back9par;
+
+    await course.postTee(courseid, name, color, text, numberofholes, rating, slope, par, 
+        front9rating, front9slope, front9par, back9rating, back9slope, back9par);
+
+    res.status(201).json({tee: tee}, {course: courseid});
+});
+
 app.listen(port, () => console.log(`Server is running on port ${port}`));
